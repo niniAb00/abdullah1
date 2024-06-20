c2b7d2195cc16a5c3a45b561a148507987dcc947
Hanin <haninalmolaiky4@gmail.com>
2024-06-20 19:00:02 +0300

    hi

---
+++ app/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { navItems } from "@/data";
+
+import Hero from "@/components/Hero";
+import Grid from "@/components/Grid";
+import { FloatingNav } from "@/components/ui/FloatingNavbar";
+
+const Home = () => {
+  return (
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <FloatingNav navItems={navItems} />
+        <Hero />
+        <Grid />
+      </div>
+    </main>
+  );
+};
+
+export default Home;
